bddc412f9a04e2dc63b3853b541532484791dca4
Krishna Tulsyan <kt.krishna.tulsyan@gmail.com>
2018-11-29 11:10:30 +0530

    added shell commands to populate pages and books database

--- searchengine/models.py
+++ searchengine/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    isbn = models.CharField(max_length=20, blank=True)
     author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=20, blank=True)
     publisher = models.CharField(max_length=200, blank=True)
     editor = models.CharField(max_length=200, blank=True)
     description = models.TextField(max_length=5000, blank=True)
@@ -16,3 +16,9 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Page(models.Model):
+    # pagetitle stores the name of the text file from which the body contents come from.
+    pagetitle = models.CharField(max_length=100, default='')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='pages')
+    content = models.TextField(max_length=5000)
--- shell.py
+++ shell.py
@@ -14,3 +14,39 @@ setup_test_environment()
 
 from django.test import Client
 c = Client()
+
+def populate_user_database():
+    os.chdir('./searchengine/static/searchengine/books')
+    for (root, _, files) in os.walk('.'):
+        if root == '.':
+            continue
+        print(root.strip())
+        print(len(files))
+        print('-------------------')
+        booktitle = root.strip('./ ')
+        bookauthor = 'krishna'
+        b = Book(title=booktitle, author=bookauthor)
+        b.save()
+        print('Processing the Pages of {}'.format(booktitle))
+        for page in files:
+            filepath = '{}/{}'.format(root.strip(), page.strip())
+            print(filepath)
+            with open(filepath, 'r') as f:
+                r = f.readlines()
+            r = [i.strip() for i in r]
+            content = '\n'.join(r)
+            if len(content)>5000:
+                print('number of characters extended beyond 5000 to {} for {}'.format(len(content), filepath))
+                return 0
+            Page(pagetitle=page.strip(), content=content, book=b).save()
+    os.chdir('../../../../')
+
+def flush_user_database():
+    p = Page.objects.all()
+    if len(p):
+        for i in p:
+            i.delete()
+    b = Book.objects.all()
+    if len(b):
+        for i in b:
+            i.delete()
